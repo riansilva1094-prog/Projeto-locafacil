6146568c0ec1976b465dfb910c3d1806fc8a3dc5
RIAN <riansilva1094@gmail.com>
2026-07-24 20:53:21 -0300

    Add files via upload

--- Loca-facil/javas.js
+++ Loca-facil/javas.js
@@ -193,3 +193,35 @@ window.addEventListener("load", () => {
     console.log("Bem-vindo à LocaFácil!");
 
 });
+
+//=====================================
+// MODO CLARO/ESCURO
+
+const button = document.getElementById("theme-toggle");
+const icon = document.getElementById("icon");
+
+// Verifica se existe tema salvo
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark");
+    icon.textContent = "🌙";
+
+}
+
+button.addEventListener("click", ()=>{
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+
+        icon.textContent = "🌙";
+        localStorage.setItem("theme","dark");
+
+    }else{
+
+        icon.textContent = "☀";
+        localStorage.setItem("theme","light");
+
+    }
+
+});
